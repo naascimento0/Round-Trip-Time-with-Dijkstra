2d38491cc00c6016082a54be0738938af996fbbd
naascimento0 <gabrieloliveiran@hotmail.com>
2024-07-18 19:44:10 -0300

    update vertex id and enum

--- headers/vertex.h
+++ headers/vertex.h
@@ -1,12 +1,24 @@
 #ifndef VERTEX_H_
 #define VERTEX_H_
 
+#include <stdio.h>
+#include <stdlib.h>
+
 typedef struct Vertex Vertex;
 
 enum VertexType{
     CLIENT = 'C',
     MONITOR = 'M',
-    SERVER = 'S'
+    SERVER = 'S',
+	REGULAR = 'R'
 };
+
+Vertex* vertexInitialize(int id, char type);
+
+int getVertexId(Vertex *vertex);
+
+char getVertexType(Vertex *vertex);
+
+void vertexDestroy(Vertex *vertex);
 
 #endif
